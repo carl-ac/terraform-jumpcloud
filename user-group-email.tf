dfa54b38056e63f258d758eb14f4ff530b35a154
Carl Tsai <carl@anticapital.ai>
2025-11-12 16:15:42 +0800

    Update

--- user-group-email.tf
+++ user-group-email.tf
@@ -30,3 +30,14 @@ resource "jumpcloud_user_group_association" "dl_valhalla_t7" {
   group_id  = jumpcloud_user_group.dl_valhalla_t7.id
   object_id = "6398f958c4650500012cb630"
 }
+
+resource "jumpcloud_user_group" "dl_valhalla_dex_1" {
+  name    = "DL_valhalla_dex_1"
+  members = [data.jumpcloud_user.bob.email, data.jumpcloud_user.bohao.email, data.jumpcloud_user.powen.email]
+}
+
+resource "jumpcloud_user_group_association" "dl_valhalla_dex_1" {
+  type      = "g_suite"
+  group_id  = jumpcloud_user_group.dl_valhalla_dex_1.id
+  object_id = "6398f958c4650500012cb630"
+}
